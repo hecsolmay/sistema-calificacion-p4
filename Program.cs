ba83f6012a91a163690bfa12d4954e95b5b88d66
hecsolmay <hecsolmay@gmail.com>
2023-11-27 12:42:57 -0600

    First Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿global using problema_3.Models;
+global using problema_3.View;
+global using problema_3.Interface;
+
+
+bool flag = true;
+Inicio inicio = new Inicio();
+
+do {
+
+  flag = inicio.MostrarInicio();
+  
+} while(flag);
+
+Console.WriteLine("Hasta Luego");
